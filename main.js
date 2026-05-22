05285164a8453f1363aec476d566fc117b109b32
shafiyaarfath-stack <shafiyaarfath@gmail.com>
2026-05-22 16:03:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    strings: [
+        "Python Programmer",
+        "AI & ML Enthusiast",
+        "Basic Web Development"
+    ],
+    
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
